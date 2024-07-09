9b189adee8bdcd3b9ecee16d92f527e5b97954f8
unknown <mohamedmamdouhgeeklab@gmail.com>
2024-07-09 11:15:36 +0300

    ed

--- Scrapers/INVESTOC/Investing site/NVDA-Scrappers.js
+++ Scrapers/INVESTOC/Investing site/NVDA-Scrappers.js
@@ -4,10 +4,10 @@ const scrapeURLs = async (page) => {
   try {
     await page.goto("https://www.investing.com/equities/nvidia-corp", {
       waitUntil: "domcontentloaded",
-      timeout: 120000
+      // timeout: 120000
     });
     const URLs = await page.evaluate(() => {
-      const ScrapeList = document.querySelectorAll(".mb-12 ul li .flex .block a");
+      const ScrapeList = document.querySelectorAll("ul[data-test='new-and-analysis-list'] a[data-test='article-title-link']");
       return Array.from(ScrapeList).map(Scrape => {
         let href = Scrape.getAttribute("href");
         let title = Scrape.innerText;
@@ -28,22 +28,35 @@ const scrapeURLs = async (page) => {
 
 
 const scrapeContentFromURL = async (page, url) => {
+  console.log("URLs--------------->" + url)
+  
   try {
+    console.log("OKKKKKKKKKKKKKKKKKKKKKKKKKKK----->1");
     await page.goto(url, {
       waitUntil: "domcontentloaded",
-      timeout: 120000
+      timeout: 40000,
     });
-    const content = await page.evaluate(() => {
-    const ScrapeList = document.querySelectorAll("#article p");
-      return Array.from(ScrapeList).map(Scrape => Scrape.innerText);
-    });
-    return content.join(' ');
-  }
-    catch (error) {
-      // await browser.close();
-      console.error(`Error during content scraping from ${url}:`, error);
+    console.log("OKKKKKKKKKKKKKKKKKKKKKKKKKKK------->2");
+    try{
+      console.log("OKKKKKKKKKKKKKKKKKKKKKKKKKKK------->3");
+        const content = await page.evaluate(() => {
+        const ScrapeList = document.querySelectorAll(".article_container .article_WYSIWYG__O0uhw p");      
+          return Array.from(ScrapeList).map(Scrape => Scrape.innerText);
+        });
+        return content.join(' ');
+    }
+    catch{
+      console.error(`fe mo4kila fi  ${content}:`, error);
       throw error;
     }
+
+
+  }
+  catch (error) {
+    // await browser.close();
+    console.error(`Error during content scraping from ${url}:`, error);
+    throw error;
+  }
 };
 
 module.exports = 
