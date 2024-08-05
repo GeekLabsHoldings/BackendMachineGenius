ca4262edfced982dd2fb212afccbbba3e6596996
unknown <mohamedmamdouhgeeklab@gmail.com>
2024-08-05 15:27:05 +0300

    trim to 5 parts

--- Controllers/Free Convert Contollers/convertController.js
+++ Controllers/Free Convert Contollers/convertController.js
@@ -35,7 +35,7 @@ const convertor = async (req, res) => {
 
     await new_movie.save();
 
-    const segmentDuration = Math.ceil(videoDuration / 2);
+    const segmentDuration = Math.ceil(videoDuration / 5);
     const totalSegments = Math.ceil(videoDuration / segmentDuration);
 
     const promises = [];
